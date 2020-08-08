8293b78282c5b73250864db2987977aa4ba7a1fb
gstanding <869734632@qq.com>
2020-08-08 13:59:50 +0800

    继承并重写paint方法

--- src/com/mashibing/tank/TankFrame.java
+++ src/com/mashibing/tank/TankFrame.java
@@ -1,22 +1,27 @@
-package com.mashibing.tank;
 
+package com.mashibing.tank;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class TankFrame {
-    public static void main(String[] args) {
-        Frame frame = new Frame();
-        frame.setSize(800, 600);
-        frame.setResizable(false);
-        frame.setVisible(true);
-        frame.setTitle("tank war");
 
-        frame.addWindowListener(new WindowAdapter() {
+public class TankFrame extends Frame{
+    public TankFrame() {
+        setSize(800, 600);
+        setResizable(false);
+        setTitle("tank war");
+        setVisible(true);
+
+        addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.fillRect(200, 200, 50, 50);
     }
 }
